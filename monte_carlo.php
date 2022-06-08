79ef0cbe0e1b27296ba19266c426a65e106164f6
Komila <gismatullina402@gmail.com>
2022-06-08 15:35:35 +0200

    added new logic

--- monte_carlo.php
+++ monte_carlo.php
@@ -22,16 +22,20 @@ function immediately_buy(): array {
 }
 
 function not_buy(): array {
+    global $remarketing;
     $not_buy = rand(0, 100);
     if($not_buy > 2){
+        $remarketing += 1;
         return simulation(10); 
     }
     return newsletter();
 }
 
 function newsletter(): array {
+    global $remarketing;
     $not_buy = rand(0, 100);
     if($not_buy > 4){
+        $remarketing += 1;
         return simulation(10); 
     }
 
@@ -110,11 +114,14 @@ while($subscribers < $users_num){
     $subscribers += rand(460, 1200);
 }
 
+$remarketing_cost = $remarketing * 2.5;
 
 echo "Probability of buying the book is " . ($successful_sells / $users_num) * 100 . " %\n";
 echo "Total cost for instagram ad " . $marketing_cost . "\n";
 echo "Total cost for discounts " . $discount_cost . "(" . $discount_cost / $total_income * 100 . " %)\n";
 echo "Total users subscribed newsletter " . (100 * $discount_15_users_num) / 4 . "\n";
 echo "Number of people who bought " . $successful_sells . "\n";
+echo "How much did it cost on average to get one customer " . (($marketing_cost + $remarketing_cost) / $successful_sells) . "\n";
 echo "People who used discounts " . $discount_users . "\n";
-echo "Total income " . $total_income . "\n";
+echo "Total income " . $total_income - $marketing_cost - $remarketing_cost . "\n";
+echo "Profit per user " . (($total_income - $marketing_cost - $remarketing_cost) / $users_num) . "\n";
